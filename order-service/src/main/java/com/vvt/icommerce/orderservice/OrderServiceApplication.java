89be9f3bdd41ed4362d4f475f8be565920fc1219
Thanh Vo <thanhvv@gmail.com>
2021-04-10 13:32:35 +0700

    Add name server and load balancer.

--- order-service/src/main/java/com/vvt/icommerce/orderservice/OrderServiceApplication.java
+++ order-service/src/main/java/com/vvt/icommerce/orderservice/OrderServiceApplication.java
@@ -3,11 +3,13 @@ package com.vvt.icommerce.orderservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 //import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
 //@EnableDiscoveryClient
+@RibbonClient(name="order-service")
 public class OrderServiceApplication {
 
 	public static void main(String[] args) {
--- order-service/src/main/java/com/vvt/icommerce/orderservice/service/ProductServiceProxy.java
+++ order-service/src/main/java/com/vvt/icommerce/orderservice/service/ProductServiceProxy.java
@@ -1,16 +1,16 @@
 package com.vvt.icommerce.orderservice.service;
 
 import com.vvt.icommerce.orderservice.dto.Product;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
-@FeignClient(name="product-service", url="localhost:8000")
-//@FeignClient(name="currency-exchange-service")
+@FeignClient(name="product-service")
 //@FeignClient(name="netflix-zuul-api-gateway-server")
-//@RibbonClient(name="currency-exchange-service")
+@RibbonClient(name="product-service")
 public interface ProductServiceProxy {
     @GetMapping("/api/products")
     public List<Product> getAllProducts();
