90732ce3f803c9c7fab4a91b09d50e7bee24d7b3
Thanh Vo <thanhvv@gmail.com>
2021-04-12 17:03:15 +0700

    Add infrastructure: deploy the whole thing on Docker containers, replace H2 by Postgres.

--- product-service/src/main/java/com/vvt/icommerce/productservice/ProductServiceApplication.java
+++ product-service/src/main/java/com/vvt/icommerce/productservice/ProductServiceApplication.java
@@ -5,9 +5,11 @@ import com.vvt.icommerce.productservice.service.ProductService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ProductServiceApplication {
 
 	@Bean
