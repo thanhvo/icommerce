8041d8f4e88553279eb46b52cb64fcdcaafc9fa8
Thanh Vo <thanhvv@gmail.com>
2021-04-17 15:41:33 +0700

    Add Web Server Docker image.

--- utils/build.sh
+++ utils/build.sh
@@ -1,3 +1,20 @@
 #!/usr/bin/env bash
+
+#eureka-server
+cd ../eureka-server
+mvn clean package -DskipTests
+cp target/*.jar docker
+
+#order-service
+cd ../order-service
 ./mvnw clean package -DskipTests
 cp target/*.jar docker
+
+#product-service
+cd ../product-service
+./mvnw clean package -DskipTests
+cp target/*.jar docker
+
+# build docker containers
+cd ../docker
+docker-compose up
